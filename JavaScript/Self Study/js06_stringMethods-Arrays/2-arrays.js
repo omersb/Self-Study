9d70a34451be66fb71051470843340a410b98161
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-05-16 23:02:21 +0300

    added

--- JavaScript/Self Study/js06_stringMethods-Arrays/2-arrays.js
+++ JavaScript/Self Study/js06_stringMethods-Arrays/2-arrays.js
@@ -25,3 +25,29 @@ console.log(sayilar);
 
 //* Diziden Veri Okuma-Yazma
 //* ---------------------------------------------------------
+
+//!Okuma
+const java = diller[1];
+console.log(java);
+
+//!Diziye Yazma
+isimler[2] = 'Canan';
+console.log(isimler);
+
+//! Hata,const keyword'u ile tanımlanmış bir diziye tamamıyla bir atama yapılmaz.
+
+// isimler = ['Can', 'Canan', 'Cavidan'];
+// console.log(isimler);
+//!Uncaught TypeError: Assignment to constant variable.
+const yaslar = [22, 18, 15];
+const kisiler = ['Ahmet', 'Yilmaz', 2022, 2022 - 1970, true, 5.6, yaslar];
+console.log(kisiler);
+
+console.log(kisiler[6][1]);
+
+const sonYas = ++kisiler[6][2];
+console.log(sonYas, kisiler);
+
+//? ===========================================================
+//?  DİZİYİ DEĞİŞTİREN (MUTATOR) METOTLAR
+//? ===========================================================
