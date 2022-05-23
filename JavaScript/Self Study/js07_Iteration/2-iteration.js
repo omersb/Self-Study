2cb67463da547f20d18f2e22a0f060f9b44bef2e
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-05-23 22:17:08 +0300

    update

--- JavaScript/Self Study/js07_Iteration/2-iteration.js
+++ JavaScript/Self Study/js07_Iteration/2-iteration.js
@@ -12,15 +12,52 @@ const fiyatlar = [100, 250, 50, 89];
 fiyatlar.forEach(yazdir);
 
 function yazdir(v) {
-    console.log(v);
+  console.log(v);
 }
 
-console.log('************************');
+console.log('***************************');
 
-//! Arrow function kullanılarak çözüm
+//! Arraw function kullanılarak çözüm
 fiyatlar.forEach((value) => console.log(value));
 
-//** fiyatlar dizisindekilerin toplamini konsola yazdırıınız */
+//** Fiyatlar dizisindekilerin toplamini konsola yazdiriniz */
 let toplam = 0;
 fiyatlar.forEach((fiyat) => (toplam += fiyat));
-console.log('Toplam: ', toplam);
+console.log('Toplam:', toplam);
+
+//! NOT: forEach metodu void bir metottur.
+//! (Yani herhangi bir değer döndürmez.)
+console.log(fiyatlar.forEach((fiyat) => (toplam += fiyat))); //!undefined
+
+//*-------------- ÖRNEK -------------------
+
+let toplam1 = 0;
+
+fiyatlar.forEach((deger, indis, dizi) => {
+  toplam1 += deger;
+  console.log(`${indis}.iterasyon toplami:${toplam1}`);
+  dizi[indis] = deger + 10;
+});
+console.log(fiyatlar);
+
+//*-------------- ÖDEV -------------------
+//* fiyatlar dizisinde her bir fiyata %10 zam yapalım.
+
+
+//* ======================================================
+//*                       MAP METHOD
+//* ======================================================
+
+//*-------------- ÖRNEK -------------------
+//* names dizisin içerisindeki her ismi büyük harf olarak
+//* yeni bir diziye saklayalım.
+
+const isimler = [
+  "Ahmet",
+  "mehmet",
+  "ismet",
+  "SAFFET",
+  "Can",
+  "Canan",
+  "Cavidan",
+];
