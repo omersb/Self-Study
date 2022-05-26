dca66dc8dd805e8698715ce6d377a4d8db2887ad
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-05-26 21:01:16 +0300

    update

--- JavaScript/Class-Notes/js08_objects-newOperators/1-objects.js
+++ JavaScript/Class-Notes/js08_objects-newOperators/1-objects.js
@@ -10,4 +10,101 @@
 //? Object'lerde Key-value (property-value) yapısı kullanılır.
 //? Herhangi bir veriyi erişmemk için property (key) adı kullanılır.
 
-console.log("******* Objects *********");
+console.log('******* Objects *********');
+
+//! 3 farklı yöntem ile Object oluşturulabilir
+//* ---------------------------------------------------------
+//* 1- Object() class'ından new Operatörü ile
+//* ---------------------------------------------------------
+
+const araclar = new Object();
+araclar.marka = 'BMW';
+araclar.motor = 1.3;
+araclar.model = 2022;
+
+//! Read
+console.log(araclar);
+console.log(araclar.motor); //? . notasyon => 1.3
+console.log(araclar['model']); //? square bracket notasyonu => 2022
+
+//* ---------------------------------------------------------
+//* 2- object constructor'ı kullanarak (OOP ile ayrıntılandırılacak)
+//* ---------------------------------------------------------
+
+//* Object constructor
+function Personel(id, ad, maas) {
+  this.perId = id;
+  this.perAdi = ad;
+  this.perMaas = maas;
+  console.log(this); //! Personel objesine bağlanmıştır (binded)
+}
+
+const ahmet = new Personel(101, 'Ahmet', 75000);
+const canan = new Personel(102, 'Canan', 85000);
+console.log(ahmet, canan);
+console.log(canan.perMaas);
+console.log(ahmet.perAdi);
+console.log(this); //! window objesine bağlanmıştır
+
+//* ---------------------------------------------------------
+//* 3- Object literal (En çok tercih edilen yöntem)
+//* ---------------------------------------------------------
+
+const calisan = {
+  ad: 'Ahmet',
+  soyad: 'Yilmaz',
+  yas: 30,
+  is: 'developer',
+  diller: ['C', 'C++', 'Pyhton', 'JS'],
+  maas: 120000,
+};
+
+console.log(calisan);
+console.log(calisan.yas);
+console.log(calisan.diller); //? ['C', 'C++', 'Pyhton', 'JS']
+console.log(calisan.diller[3]); //? JS
+console.log(calisan[1]); //? undefined
+
+calisan.konum = 'Turkey';
+calisan.email = 'ahmet@gmail.com';
+calisan['dogum'] = 1990;
+console.log(calisan);
+
+const isci = calisan; //* referans aktarımı, sığ kopyalama
+isci.maas = 50000;
+console.log(isci, calisan);
+
+//* ======================================================
+//*              Object Metotları
+//* ======================================================
+
+//! Objelere, classlara ait olan fonksiyonlara metot denir.
+
+const kisi = {
+  ad: 'Can',
+  soyad: 'Canan',
+  dogum: 1990,
+  meslek: 'developer',
+  ehliyet: true,
+  yasHesapla: function () {
+    return new Date().getFullYear() - this.dogum;
+  },
+  // ozet: function () {
+  //   return `${this.ad}, ${this.yasHesapla()} yasindadir`;
+  // },
+  ozet: () => {
+    return `${this.ad}, ${this.yasHesapla()} yasindadir`;
+  },
+};
+console.log(this);
+
+console.log('YAS:', kisi.yasHesapla());
+console.log('BILGI:', kisi.ozet());
+
+// console.log('BILGI:', this.ozet());
+
+//! NOT: arrow fonksiyonları farklı amaç için geliştirilmiş fonksiyonlarıdır
+//! ve lexical context'e sahiptirler. Dolayısıyla, bir arrow fonk. içerisinde
+//! this kelimesi kullanılrsak beklenmeyen sonuclar alabiliriz.
+//! Çünkü, arrow içerisindeki this kelimesi global scope'u gösterir. (window nesnesini) gösterir.
+//! Bunu engellemek için object fonskyionlarını tanımlamak için normal fonksiyon  yöntemlerini kullanmak gerekir.
