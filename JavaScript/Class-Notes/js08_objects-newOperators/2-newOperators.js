9e3436420c8bf0d2a8542f08c8ae7b5d2d12e4aa
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-05-26 22:55:14 +0300

    update

--- JavaScript/Class-Notes/js08_objects-newOperators/2-newOperators.js
+++ JavaScript/Class-Notes/js08_objects-newOperators/2-newOperators.js
@@ -38,3 +38,106 @@ console.log(c1name, c1model);
 
 const {name: c2name, model: c2model} = car2;
 console.log(c2name, c2model);
+
+const kisiler = [
+  {
+    name: 'Ahmet',
+    surname: 'Can',
+    job: 'developer',
+    age: 30,
+  },
+  {
+    name: 'Mehmet',
+    surname: 'Baki',
+    job: 'tester',
+    age: 35,
+  },
+  {
+    name: 'Ruzgar',
+    surname: 'Kuzey',
+    job: 'tester',
+    age: 24,
+  },
+];
+
+kisiler.forEach((kisi) => {
+    console.log("***********");
+    console.log("NAME: ", kisi.name);
+    console.log("SURNAME:", kisi.surname);
+    console.log("JOB: ", kisi.job);
+    console.log("AGE: ", kisi["age"]);
+});
+
+kisiler.forEach((kisi) => {
+    console.log("***********");
+    const {name, job, surname, age} = kisi;
+    console.log("NAME: ", name);
+    console.log("SURNAME:", surname);
+    console.log("JOB: ", job);
+    console.log("AGE: ", age);
+});
+
+//* function
+function getInfo() {
+    return {
+        id: 101,
+        name: "Apple",
+        price: 20000,
+    };
+}
+
+const {id, name, price} = getInfo();
+console.log(id, price);
+
+//* ======================================================
+//*    NEW GENERATION OPERATORS: DESTRUCTURING (ARRAY)
+//* ======================================================
+
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffat"];
+
+//* Classical method */
+const name1 = names[0];
+const name2 = names[1];
+
+const [person1, person2, , person4] = names;
+console.log(person1, person2,person4);
+
+//*======================================================
+//*   NEW GENERATION OPERATORS: SPREAD OR REST (...)
+//* ======================================================
+
+//* REST: (Arrays) */
+const vehicles = ["bmw", "reno", "mercedes", "ferrari", "anadol"];
+
+const [vec1, vec2, ...restVehicles] = vehicles;
+
+console.log(vec1, vec2);
+console.log(restVehicles);
+
+//* REST: (Objects) */
+const personel = {
+    pName: "john",
+    surname: "smith",
+    job: "dev",
+    age: 30
+};
+
+const {pName, job, ...surnameAge} = personel;
+console.log(pName, job, surnameAge);
+
+//* REST(Function Arguments)
+const topla = (x, y) => {
+    return x + y;
+};
+
+//? hata vermez fakat sadece 2 argümanı toplar
+console.log(topla(1, 2, 3, 4, 5, 6));
+
+const hepsiniTopla = (...sayilar) =>{
+    let toplam = 0;
+    console.log(sayilar);
+    sayilar.forEach((sayi) => toplam += sayi);
+    return toplam;
+};
+
+console.log(hepsiniTopla(1, 2, 3, 4, 5, 6));
