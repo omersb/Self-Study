1f598ce842cbaa46538d0ea476bc5f19d21f54f9
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-05-27 23:08:11 +0300

    update

--- JavaScript/Example/e002.js
+++ JavaScript/Example/e002.js
@@ -3,18 +3,32 @@
 // let numara = document.getElementById("numara").innerHTML
 // let roman = document.getElementById("roma").innerHTML
 
-function convertToRoman(num) {
+// function convertToRoman(num) {
+//     let roman = '';
+//     let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+//     let romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+//     for (let i = 0; i < decimal.length; i++) {
+//         while (decimal[i] <= num) {
+//             roman += romanNum[i];
+//             num -= decimal[i];
+//         }
+//     }
+//     return roman;
+// } 
+
+// let sayı = prompt("Bir sayı giriniz: ")
+// console.log(convertToRoman(sayı));   
+
+    let num = 2770;
     let roman = '';
     let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     let romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    for (let i = 0; i < decimal.length; i++) {
-        while (decimal[i] <= num) {
-            roman += romanNum[i];
-            num -= decimal[i];
-        }
-    }
-    return roman;
-} 
-
-// let sayı = prompt("Bir sayı giriniz: ")
-console.log(convertToRoman(sayı));   
+    let bolum =0;
+    for (let i = 0; i < decimal.length; i++){
+        bolum = Math.floor(num / decimal[i]);
+        num = num % decimal[i];
+        for(let k = 0; k < bolum; k++){
+            roman += romanNum[i]
+        }        
+        console.log(bolum);
+    }console.log(roman);
