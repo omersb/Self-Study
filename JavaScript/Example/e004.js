f3ae4a68ee8934a6a6dcde3498a85ead44c1336d
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-03 23:20:07 +0300

    Answer

--- JavaScript/Example/e004.js
+++ JavaScript/Example/e004.js
@@ -1,7 +1,9 @@
-// JS-CC-007 : Count Digits
-// Suppose we have an integer d between 0 and 9, we also have two positive integers low and high as lower and upper bounds, respectively. We have to find the number of times that d occurs as a digit in all integers between low and high, including the bounds low and high.
+//? JS-CC-007 : Count Digits
+//? Suppose we have an integer d between 0 and 9, we also have two positive integers low and high as lower and upper bounds, respectively. We have to find the number of times that d occurs as a digit in all integers between low and high, including the bounds low and high.
 
-// For example, d=2, low= 10, high = 23 then the output will be 6, as digit d = 2 occurs 6 times: 12, 20, 21, 22, 23.
+//? For example, d=2, low= 10, high = 23 then the output will be 6, as digit d = 2 occurs 6 times: 12, 20, 21, 22, 23.
+
+//***** Answer *****//
 console.log(countDigits(5,30, 39));
 
 function countDigits(digit, low, high) { 
