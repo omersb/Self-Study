fe1161f392bed8869c8b9366a581c63fd0e55043
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-03 23:03:04 +0300

    Count Digits

---
+++ JavaScript/Example/e004.js
@@ -0,0 +1,10 @@
+// JS-CC-007 : Count Digits
+// Suppose we have an integer d between 0 and 9, we also have two positive integers low and high as lower and upper bounds, respectively. We have to find the number of times that d occurs as a digit in all integers between low and high, including the bounds low and high.
+
+// For example, d=2, low= 10, high = 23 then the output will be 6, as digit d = 2 occurs 6 times: 12, 20, 21, 22, 23.
+
+function countDigits(digit, low, high) { 
+    
+    // returning value should be int type.
+    return null;
+}
