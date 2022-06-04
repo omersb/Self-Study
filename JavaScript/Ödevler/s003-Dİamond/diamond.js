0fa59563cc3e3c56160aad36e2a9b0826b7b845a
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-04 12:38:40 +0300

    Draw Diamond with Star Character

---
+++ JavaScript/Ödevler/s003-Dİamond/diamond.js
@@ -0,0 +1,8 @@
+//? JS-CC-010 : Draw Diamond with Star Character
+//? The purpose of this coding challenge is to draw an empty diamond figure (only the outline) into textarea field, using star character (*). Take the number of rows from user and draw the diamond figure accordingly. Add event listener to the input field and call the function when the user enters or changes the value.
+//! Learning Outcomes
+//! At the end of the this coding challenge, students will be able to;
+//! Analyze a problem, identify and apply programming knowledge for appropriate solution.
+//! Demonstrate their knowledge of algorithmic design principles by using JavaScript effectively.
+//! Use the DOM to manipulate the page.
+//! Become familiar with the DOM Events API.
