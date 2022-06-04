e50490a9cc83d4728c2cc13ccd079e66de38942b
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-04 12:51:50 +0300

    update

--- JavaScript/Ödevler/s003-Dİamond/diamond.js
+++ JavaScript/Ödevler/s003-Dİamond/diamond.js
@@ -6,3 +6,43 @@
 //! Demonstrate their knowledge of algorithmic design principles by using JavaScript effectively.
 //! Use the DOM to manipulate the page.
 //! Become familiar with the DOM Events API.
+
+function diamond(row) {
+  s = '';
+  // upper
+  for (i = 1; i < row + 1; i++) {
+    s += '\n';
+    for (j = 1; j <= row - i + 1; j++) {
+      s += ' ';
+    }
+    for (j = 1; j <= 2 * i - 1; j++) {
+      if (j == 1 || j == 2 * i - 1) {
+        s += '*';
+      } else {
+        s += ' ';
+      }
+    }
+    for (j = 1; j <= row - i + 1; j++) {
+      s += ' ';
+    }
+  }
+  // bellow
+  for (i = row - 1; i > 0; i--) {
+    s += '\n';
+    for (j = 1; j <= row - i + 1; j++) {
+      s += ' ';
+    }
+    for (j = 1; j <= 2 * i - 1; j++) {
+      if (j == 1 || j == 2 * i - 1) {
+        s += '*';
+      } else {
+        s += ' ';
+      }
+    }
+    for (j = 1; j <= row - i + 1; j++) {
+      s += ' ';
+    }
+  }
+  return s;
+}
+console.log(diamond(5));
