fe72ad8fe086ed30d6fe0be2569882fa3936138a
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-14 23:09:26 +0300

    fetch update

--- JavaScript/Class-Notes/js15_async1/3-fetchAPI.js
+++ JavaScript/Class-Notes/js15_async1/3-fetchAPI.js
@@ -11,4 +11,24 @@
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
 fetch('https://api.github.com/users')
-.then((res) => console.log(res));
+  .then((res) => {
+    //! error handling
+    if (!res.ok) {
+      throw new Error(`Something went wrong: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => updateDom(data))
+  .catch((err) => console.log(err));
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector('.users');
+
+  data.forEach((user) => {
+    const { login, avatar_url, html_url } = user;
+    userDiv.innerHTML += `
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />;
+  `;
+  });
+};
