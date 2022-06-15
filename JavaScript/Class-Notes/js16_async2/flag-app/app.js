51039b738f3f8a4d01dd576835f790724e76eb3a
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-15 23:01:33 +0300

    update

--- JavaScript/Class-Notes/js16_async2/flag-app/app.js
+++ JavaScript/Class-Notes/js16_async2/flag-app/app.js
@@ -28,10 +28,47 @@ const renderError = (err) => {
 const renderCountry = (country) => {
   console.log(country);
   const countriesDiv = document.querySelector('.countries');
-  const { capital } = country;
-  console.log(capital);
+
+  //!destr
+  const {
+    capital,
+    name: { common },
+    region,
+    flags: { svg },
+    languages,
+    currencies,
+  } = country;
+
+  // console.log(capital, common, region, svg);
+  // console.log(Object.values(languages));
+  // console.log(Object.values(currencies)[0].name);
+  // console.log(Object.values(currencies)[0].symbol);
+
+  countriesDiv.innerHTML += `
+
+  <div class="card shadow-lg" style="width: 18rem;">
+    <img src="${svg}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${common}</h5>
+      <p class="card-text">${region}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${capital}</li>
+      <li class="list-group-item"> <i class="fas fa-lg fa-comments"></i> ${Object.values(
+        languages
+      )}</li>
+
+      <li class="list-group-item"> <i class="fas fa-lg fa-money-bill-wave"></i> ${
+        Object.values(currencies)[0].name
+      }, ${Object.values(currencies)[0].symbol} </li>
+
+    </ul>
+  </div>
+
+  `;
 };
 
 fetchCountry('turkey');
-// fetchCountry('usa');
-// fetchCountry('france');
+fetchCountry('usa');
+fetchCountry('belgium');
+fetchCountry('south africa');
