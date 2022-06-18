1dfc9b72a43f60e97442a89f10356efdbffe2466
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-18 11:50:29 +0300

    Constructor sınıf oluşturma

--- JavaScript/Class-Notes/js18_oop1/2-ObjectConstructor.js
+++ JavaScript/Class-Notes/js18_oop1/2-ObjectConstructor.js
@@ -8,3 +8,24 @@
 //* Object prototipi, prototip miras zincirinin en tepesinde yer almaktadir.
 //* Ornegin Date and Array nesneleri Object.prototype'dan miras almaktadir.
 
+//? Object Constructor
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function () {
+    return `${this.title} was written by ${this.author} in ${this.year} `;
+  };
+}
+
+//? new keyword'u Book Constructor'ini parametereler ile cagirmaktadir.
+//? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
+//? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
+//? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan
+//? her bir instance'a da hayat bulmus olur.
+
+const book1 = new Book("Kasagi", "Omer Seyfettin", 1919);
+const book2 = new Book("Sinekli Bakkal", "H.Edip Adıvar", 1910);
+
+console.log(book1);
+console.log(book2.getSummary());
