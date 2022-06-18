c64d86e95f683013384dc41c6dd4c164aa0fab6f
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-18 13:41:16 +0300

    update

--- JavaScript/Class-Notes/js18_oop1/2-ObjectConstructor.js
+++ JavaScript/Class-Notes/js18_oop1/2-ObjectConstructor.js
@@ -47,11 +47,11 @@ console.log(book1.getAge());
 console.log(book2.getAge());
 console.log(book2.type);
 
-//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir
-//! sekilde metotlar tanimlamaya izin vermektedir.
-//! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un olusan
-//! tum instance'lerin belleginde yer kaplamamasi ancak tum instance'larin bu metota
-//! ulasabilmesidir.
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina
+//! kolay bir sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin guzelligi bu metot'un
+//! olusan tum instance'lerin belleginde yer kaplamamasi ancak tum
+//! instance'larin bu metota ulasabilmesidir.
 
 //* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
 //* Ancak, getAge() fonksiyonu bellekte sadece tek bir yer kaplamaktadir.
@@ -64,3 +64,25 @@ console.log(Book.prototype);
 
 //!instance'larin proto alanini gormk icin .__proto__
 console.log(book1.__proto__);
+
+//? INHERITANCE (Kalitim - ES5)
+//?-------------------------------------------------------------------
+
+function Magazine(title, author, year, month) {
+  //* parent'in object constructorinin cagrilmasi
+  Book.call(this, title, author, year);
+  this.month = month;
+}
+
+//! Prototipler dogrudan miras olarak gelmez.
+console.log(Magazine.prototype);
+
+//! Prototipleri miras almak icin Object.create() metodu kullanabilir.
+Magazine.prototype = Object.create(Book.prototype);
+console.log(Magazine.prototype);
+
+//* Magazine nesnesinden bir instance olsuturulmasi
+const mag1 = new Magazine("Scientific Research", "Einstein", 1905, "September");
+
+console.log(mag1);
+console.log(mag1.getAge());
