57f1b73ec6251d5ee02f72ebfc9a7b5fd53db2e0
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-22 21:54:08 +0300

    css added

--- React/Class-Notes/rc02_jsx_basicStyling/src/Content.jsx
+++ React/Class-Notes/rc02_jsx_basicStyling/src/Content.jsx
@@ -8,11 +8,27 @@
 //* property isimlerinde camelCase kullanimalidir.
 //* class isimleri icin className kullanilir.
 //* Material UI, Styled Component, Sass, Bootstrap v.b harici kutuphaneler kullanilabilir.
-
+import React from "react";
+import img2 from "./img/spring2.jpg";
+//dahili resim eklemek için import yapmak gereklidir.
 const Content = () => {
   return (
-    <div>Content</div>
-  )
-}
+    <div>
+      {/* inline style */}
+      <h2 style={{ color: "red" }}>React Js </h2>
 
-export default Content
+      <p style={{ backgroundColor: "lightgreen" }}>
+        React, Kullanıcı arabirimi tasarlamak için kullanılan bir Js
+        kütüphanesidir.
+      </p>
+
+      <img
+        src="https://cdn.pixabay.com/photo/2017/02/15/13/40/tulips-2068692__340.jpg"
+        alt="Spring-Flower"
+      />
+      <img src={img2} alt="img2" />
+    </div>
+  );
+};
+
+export default Content;
