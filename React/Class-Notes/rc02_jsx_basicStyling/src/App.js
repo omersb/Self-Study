78c823746da4a4d7e7d8ab765bd8972619269059
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-22 20:47:46 +0300

    header update

--- React/Class-Notes/rc02_jsx_basicStyling/src/App.js
+++ React/Class-Notes/rc02_jsx_basicStyling/src/App.js
@@ -1,12 +1,28 @@
+//? React,JSX kullanmaktadir.
+//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+
 import Header from "./Header";
+
+//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
+//! bir kod parcacigidir.
+
+//! Bir componentin return () kismi ise JSX kodlari icerir.
+
+//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
+//! component eger birden fazla html elementi dondurmesi
+//! gerekirse bunlari tek bir container elemani ile sarmayalarak
+//! dondurmelidir.container icin div, article, header, <> v.b
+//! herhangi bir element kullanilabilir.
 
 //? App componentinin tanimlanmasi
 function App() {
+  //* JSX
+  //? stillendirme yapilmayacaksa container icin <> kullanilabilir
   return (
-    <div>
+    <>
       <Header />
       <h1>APP</h1>
-    </div>
+    </>
   );
 }
 
