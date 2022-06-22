320539cc6695fed31fd5063152232ffc8f7f0c41
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-22 22:52:32 +0300

    props

--- React/Class-Notes/rc02_props/src/Msg.jsx
+++ React/Class-Notes/rc02_props/src/Msg.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
 const Msg = (props) => {
-    // console.log(props);
-    const {name} = props;
-    return (
-        <div>
-            <h3>Merhaba {name}</h3>
-        </div>
-    )
-}
+  // console.log(props);
 
-export default Msg
+  //! Destructiring
+  const { name } = props;
+
+  return (
+    <div>
+      <h3>Merhaba {name}</h3>
+    </div>
+  );
+};
+
+export default Msg;
