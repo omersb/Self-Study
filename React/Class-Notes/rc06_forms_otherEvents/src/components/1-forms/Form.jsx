6c1d35ba1cb15a0eb2a90a8b2ba32f2a35bd0f1f
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-28 20:53:34 +0300

    update

--- React/Class-Notes/rc06_forms_otherEvents/src/components/1-forms/Form.jsx
+++ React/Class-Notes/rc06_forms_otherEvents/src/components/1-forms/Form.jsx
@@ -1,26 +1,56 @@
+import { useState } from "react";
 const Form = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleUsername = (event) => {
+    console.log(event.target.value);
+    setUsername(event.target.value);
+  };
+  const handleSubmit = () => {
+    alert(`username${username}
+  email:${email}
+  password:${password}`);
+  };
   return (
     <div>
-      <form style={{ margin: "5rem" }}>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
-            Username
+            Username: <span className="text-danger">{username}</span>
           </label>
-          <input type="text" className="form-control" id="username" />
+          <input
+            onChange={handleUsername}
+            value={username}
+            type="text"
+            className="form-control"
+            id="username"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
-            Email address
+            Email address: <span className="text-danger">{email}</span>
           </label>
-          <input type="email" className="form-control" id="email" />
+          <input
+            onChange={(event) => setEmail(event.target.value)}
+            value={email}
+            type="email"
+            className="form-control"
+            id="email"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type="password" className="form-control" id="password" />
+          <input
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+            type="password"
+            className="form-control"
+            id="password"
+          />
         </div>
-
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
@@ -28,5 +58,4 @@ const Form = () => {
     </div>
   );
 };
-
 export default Form;
