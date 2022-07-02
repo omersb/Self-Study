c8a9578904639cd54348cdbe08c133efa7d64cce
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-02 11:55:29 +0300

    update

--- React/Class-Notes/rc09_useEffect-axios-example/src/pages/Home.jsx
+++ React/Class-Notes/rc09_useEffect-axios-example/src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import AddTutorial from '../components/AddTutorial';
-import TutorialList from '../components/TutorialList';
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState();
+
+  const url = " https://cw-axios-example.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    const { data } = await axios.get(url);
+    setTutorials(data);
+  };
+
+  //! Sadece Component mount oldugunda istek yapar
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  console.log(tutorials);
 
   return (
     <>
