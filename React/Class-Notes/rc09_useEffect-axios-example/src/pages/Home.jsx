bfb4e5eab2caf5993d3c2dbb24ee9f4d575e9593
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-02 10:34:11 +0300

    useEffect added

---
+++ React/Class-Notes/rc09_useEffect-axios-example/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import AddTutorial from '../components/AddTutorial';
+import TutorialList from '../components/TutorialList';
+
+const Home = () => {
+
+  return (
+    <>
+      <AddTutorial />
+      <TutorialList />
+    </>
+  );
+};
+
+export default Home;
