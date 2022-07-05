6ccddd394fe8298bd2b01a1a35c2009fc9361000
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-05 21:03:43 +0300

    link

--- React/Class-Notes/rc11_styled-components-intro/src/App.js
+++ React/Class-Notes/rc11_styled-components-intro/src/App.js
@@ -1,6 +1,7 @@
 import Button, { TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
 import HeaderText from "./components/Header.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <TomatoButton>Click</TomatoButton>
         <TomatoButton primary>Click</TomatoButton>
         <HeaderText color="white"> STYLED COMPONENTS</HeaderText>
+        <StyledLink href="https://clarusway.com/" target="_blank">Clarusway</StyledLink>
       </Container>
     </>
   );
