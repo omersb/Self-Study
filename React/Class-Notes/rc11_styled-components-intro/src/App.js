a87386e77475b49e19beb9515f24893b39d58d76
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-05 20:56:12 +0300

    added

--- React/Class-Notes/rc11_styled-components-intro/src/App.js
+++ React/Class-Notes/rc11_styled-components-intro/src/App.js
@@ -1,12 +1,18 @@
 import Button, { TomatoButton } from "./components/Button.styled";
+import Container from "./components/Container.styled";
+import HeaderText from "./components/Header.styled";
 
 const App = () => {
   return (
     <>
-      <Button primary>Click</Button>
-      <Button>Click</Button>
-      <TomatoButton>Click</TomatoButton>
-      <TomatoButton primary>Click</TomatoButton>
+      <Container bg="green">
+        <HeaderText> STYLED COMPONENTS</HeaderText>
+        <Button primary>Click</Button>
+        <Button>Click</Button>
+        <TomatoButton>Click</TomatoButton>
+        <TomatoButton primary>Click</TomatoButton>
+        <HeaderText color="white"> STYLED COMPONENTS</HeaderText>
+      </Container>
     </>
   );
 };
---
+++ React/Class-Notes/rc11_styled-components-intro/src/components/Container.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: ${({ bg }) => bg || "white"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 0.7rem;
+`;
+
+export default Container;
