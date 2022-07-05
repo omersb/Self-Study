4438bc3f0c3a5144562e1b45e79a9924008d7640
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-05 22:30:08 +0300

    header, container

--- React/Class-Notes/rc11_styled-example/src/components/Header.jsx
+++ React/Class-Notes/rc11_styled-example/src/components/Header.jsx
@@ -1,13 +1,19 @@
 import Button from "./styles/Button.styled";
-import StyledHeader from "./styles/Header.style";
+import Container from "./styles/Container.styled";
+import StyledHeader, { Logo, Nav } from "./styles/Header.style";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <div>
-        <Button color="#A62440">Apply Courses</Button>
-        <Button bg="#A62440">Talk to Adviser</Button>
-      </div>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.png"></Logo>
+          <div>
+            <Button color="#A62440">Apply Courses</Button>
+            <Button bg="#A62440">Talk to Advisor</Button>
+          </div>
+        </Nav>
+      </Container>
     </StyledHeader>
   );
 };
--- React/Class-Notes/rc11_styled-example/src/components/styles/Header.style.jsx
+++ React/Class-Notes/rc11_styled-example/src/components/styles/Header.style.jsx
@@ -1,7 +1,20 @@
+// esch 👇
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
+  background-color: ${({ theme }) => theme.colors.header};
   padding: 4rem 0;
+`;
+
+export const Logo = styled.img`
+  width: 300px;
+  padding: 4rem 0;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export default StyledHeader;
