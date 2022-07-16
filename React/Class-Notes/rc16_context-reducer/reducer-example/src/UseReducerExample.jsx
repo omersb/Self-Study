b75add97123827de69dc143dca89f6ff55482947
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-16 14:09:51 +0300

    reducer

--- React/Class-Notes/rc16_context-reducer/reducer-example/src/UseReducerExample.jsx
+++ React/Class-Notes/rc16_context-reducer/reducer-example/src/UseReducerExample.jsx
@@ -1,32 +1,32 @@
-import { useReducer, useState } from "react";
-import { initialState, reducer } from "./reducer";
+import { useReducer } from 'react';
+import { initialState, reducer } from './reducer';
 
 const UserReducerExample = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { loading, error, dog } = state;
+  
   const fetchDog = () => {
-    dispatch({ type: "START" });
-
-    fetch("https://dog.ceo/api/breeds/image/random")
+    dispatch({ type: 'START' });
+    fetch('https://dog.ceo/api/breeds/image/random')
       .then((res) => res.json())
       .then((data) => {
-        dispatch({ type: "SUCCESS", payload: data.message });
+        dispatch({ type: 'SUCCESS', payload: data.message });
       })
       .catch(() => {
-        dispatch({ type: "FAİL", payload: "ERROR!! DATA CAN NOT BE FETCHED" });
+        dispatch({ type: 'FAIL', payload: 'ERROR!! DATA CAN NOT BE FETCHED' });
       });
   };
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
       <button
         onClick={fetchDog}
         disabled={loading}
-        style={{ width: "200px", margin: "1rem" }}
+        style={{ width: '200px', margin: '1rem' }}
       >
         Fetch Dog
       </button>
--- React/Class-Notes/rc16_context-reducer/reducer-example/src/reducer.jsx
+++ React/Class-Notes/rc16_context-reducer/reducer-example/src/reducer.jsx
@@ -1,17 +1,17 @@
 export const initialState = {
   loading: false,
-  dog: "",
-  error: "",
+  dog: '',
+  error: '',
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case "START":
-      return { ...state, dog: "", error: "", loading: true };
-    case "SUCCESS":
-      return { ...state, dog: action.payload, error: "", loading: false };
-    case "FAİL":
-      return { ...state, dog: "", error: action.payload, loading: false };
+    case 'START':
+      return { ...state, dog: '', error: '', loading: true };
+    case 'SUCCESS':
+      return { ...state, dog: action.payload, error: '', loading: false };
+    case 'FAIL':
+      return { ...state, dog: '', error: action.payload, loading: false };
     default:
       break;
   }
