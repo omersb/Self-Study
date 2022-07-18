2f36870361e6e61c3dcc9f6b1ef2777d7260d35f
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-18 21:29:03 +0300

    update

--- React/Class-Notes/rc17_material-ui/material-intro/src/components/TypoButtons.jsx
+++ React/Class-Notes/rc17_material-ui/material-intro/src/components/TypoButtons.jsx
@@ -1,5 +1,52 @@
+import { Typography, Container, Button } from '@mui/material';
 const TypoButtons = () => {
-  return <div>TypoButtons</div>;
+  return (
+    <div>
+      <Container maxWidth="md">
+        <Typography
+          variant="h4"
+          component="h6"
+          align="center"
+          color="error"
+          sx={{
+            background: '#eee',
+            mt: 4,
+            border: '1px solid green',
+          }}
+        >
+          Typography, Buttons, Container, Box
+        </Typography>
+
+        <Typography variant="body2" align="justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          deserunt atque fugiat ratione blanditiis earum sed tempora quas
+          voluptas laudantium molestias enim omnis exercitationem culpa labore,
+          velit facilis nobis alias!
+        </Typography>
+
+        <Typography
+          variant="button"
+          align="justify"
+          color="secondary"
+          sx={{ display: 'inline-block', mt: 4 }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          deserunt atque fugiat ratione blanditiis earum sed tempora quas
+          voluptas laudantium molestias enim omnis exercitationem culpa labore,
+          velit facilis nobis alias!
+        </Typography>
+      </Container>
+
+      <Container maxWidth="md">
+        <Button variant="contained" color="secondary">
+          CLICK
+        </Button>
+        <Button variant="outlined" color="success">
+          SEND
+        </Button>
+      </Container>
+    </div>
+  );
 };
 
 export default TypoButtons;
