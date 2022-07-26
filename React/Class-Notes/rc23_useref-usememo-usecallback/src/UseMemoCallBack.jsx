68589b8c1c65bde3c7811ab360997c32346d3f48
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-26 22:49:22 +0300

    update

--- React/Class-Notes/rc23_useref-usememo-usecallback/src/UseMemoCallBack.jsx
+++ React/Class-Notes/rc23_useref-usememo-usecallback/src/UseMemoCallBack.jsx
@@ -1,17 +1,34 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Users from './components/Users';
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import Users from "./components/Users";
 
 const UseMemoCallBack = () => {
   const [users, setUsers] = useState([]);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+  console.log("text", text, "search", search);
+  //   const inputRef = useRef();
+
   useEffect(() => {
     axios
-      .get('https://jsonplaceholder.typicode.com/users')
+      .get("https://jsonplaceholder.typicode.com/users")
       .then((res) => setUsers(res.data));
   }, []);
-  console.log(users);
+  //   console.log(users);
+
+  const handleSearch = () => {
+    setSearch(text);
+    // setSearch(inputRef.current.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
+      <input type="search" onChange={(e) => setText(e.target.value)} />
+      <button onClick={{ handleSearch }}>Search User</button>
       <Users users={users} />
     </div>
   );
