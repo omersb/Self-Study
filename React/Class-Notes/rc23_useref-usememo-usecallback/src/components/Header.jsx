8e87893dcc0a743767cf9af7958229052cb36fbf
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-07-26 21:51:14 +0300

    update

--- React/Class-Notes/rc23_useref-usememo-usecallback/src/components/Header.jsx
+++ React/Class-Notes/rc23_useref-usememo-usecallback/src/components/Header.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { memo } from 'react';
 import cw from '../assets/cw_logo.png';
+
+//* React.memo componentleri hafızaya almak için kullanılır, render'ı önler
 
 const Header = ({ img }) => {
   console.log('Rendering: Header Comp.');
@@ -14,4 +16,38 @@ const Header = ({ img }) => {
   );
 };
 
-export default Header;
+export default memo(Header);
+
+//* 2.kullanım
+// const Header = memo(({ img }) => {
+
+//   console.log('Rendering: Header Comp.');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="cw_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// });
+
+// export default Header;
+
+//* 1.kullanım
+// const Header = React.memo(({ img }) => {
+
+//   console.log('Rendering: Header Comp.');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="cw_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// });
+
+// export default Header;
