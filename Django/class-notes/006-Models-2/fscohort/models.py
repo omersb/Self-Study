dae6b5b467ddb32d2c7af1b660732eadb8f5f92e
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-08-22 21:20:29 +0300

    update

--- Django/class-notes/006-Models-2/fscohort/models.py
+++ Django/class-notes/006-Models-2/fscohort/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField('Adı', max_length=50)
+    last_name = models.CharField('Soyadı', max_length=50)
+    number = models.IntegerField('Numara')
+
+    def __str__(self):
+        return f"{self.number} - {self.first_name} {self.last_name}"
+
+    # https://docs.djangoproject.com/en/4.1/ref/models/options/#model-meta-options
+    class Meta:
+        ordering = ['number'] # DESC -> ['-number']
+        verbose_name_plural = 'Öğrenciler'
