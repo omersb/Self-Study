acb8671c78eb98158aec0cf2484bcdab441464a7
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-03 12:17:32 +0300

    update heroku

--- Django/Project-Solutions/todo/main/settings.py
+++ Django/Project-Solutions/todo/main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
