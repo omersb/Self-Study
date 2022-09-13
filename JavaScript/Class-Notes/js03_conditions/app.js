d20101d6427c55a83bf3b140a2e4befad1614991
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-13 20:21:52 +0300

    update

--- JavaScript/Class-Notes/js03_conditions/app.js
+++ JavaScript/Class-Notes/js03_conditions/app.js
@@ -1,6 +1,6 @@
 console.log("**********\nSession 3 - Conditionals\n**********");
 // ! if statement example
-//* check if the is positive
+//? check if the is positive
 
 // const n1 = +prompt("Enter a number:");
 // console.log(n1, typeof n1);
@@ -16,24 +16,28 @@ console.log("**********\nSession 3 - Conditionals\n**********");
 
 // https://www.perryellis.com/pages/size-chart
 
-// suggest size for men as XS S M L XL XXL on chest size
+//? suggest size for men as XS S M L XL XXL on chest size
 
-const size = +prompt("beden ölçünüz giriniz:");
+// const size = +prompt("beden ölçünüz giriniz:");
 
-if (size < 32) {
-  console.log("Wrong size");
-} else if (size < 35) {
-  console.log("XS");
-} else if (size < 38) {
-  console.log("S");
-} else if (size < 41) {
-  console.log("M");
-} else if (size < 44) {
-  console.log("L");
-} else if (size < 47) {
-  console.log("XL");
-} else if (size < 50) {
-  console.log("XXL");
-} else {
-  console.log("Wrong Size");
-}
+// if (size < 32) {
+//   console.log("Wrong size");
+// } else if (size < 35) {
+//   console.log("XS");
+// } else if (size < 38) {
+//   console.log("S");
+// } else if (size < 41) {
+//   console.log("M");
+// } else if (size < 44) {
+//   console.log("L");
+// } else if (size < 47) {
+//   console.log("XL");
+// } else if (size < 50) {
+//   console.log("XXL");
+// } else {
+//   console.log("Wrong Size");
+// }
+
+// ! example 2 three number comparison
+
+//? Write a JavaScript program that accept three integers and display the greatest.
