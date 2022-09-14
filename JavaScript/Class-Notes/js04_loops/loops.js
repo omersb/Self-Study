3c45a1e73a4e7ef7589c9b7c9a4baff71a3ab2b8
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-14 09:50:17 +0300

    update

--- JavaScript/Class-Notes/js04_loops/loops.js
+++ JavaScript/Class-Notes/js04_loops/loops.js
@@ -97,3 +97,28 @@ console.log("****** LOOPS *******");
 // }
 
 // console.log('Q ya basarak çıkış yaptınız !!!');
+
+var sum = 0;
+for (var i = 3; i < 7; i++) {
+  sum += i;
+  if (i === 3 || sum > 10) {
+    continue;
+  }
+  console.log(i);
+}
+
+// var players = ["Messi ", "Ronaldo ", "Hazard ", "Neymar ", "Salah "];
+// var text = "";
+// var i;
+// for (i = 2; i < players.length; i++) {
+//   text += players[i];
+// }
+// console.log(text);
+
+// var i = 0;
+// var sum = 0;
+// while (i < 3) {
+//   var sum = sum + i;
+//   i++;
+// }
+// console.log("The total is: " + sum);
