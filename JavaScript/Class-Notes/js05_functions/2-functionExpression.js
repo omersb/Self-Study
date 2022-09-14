1d8a55d09d192ee7dc717edb99d2e1e6f61593c2
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-14 13:23:05 +0300

    update

--- JavaScript/Class-Notes/js05_functions/2-functionExpression.js
+++ JavaScript/Class-Notes/js05_functions/2-functionExpression.js
@@ -5,12 +5,14 @@
 //!----------------------------------------------------------
 //! 2.YÖNTEM  : FUNCTION EXPRESSION
 //!-----------------------------------------------------------
+
 //* Örnek1:
 //**********************************************************/
-console.log('******** 2- EXPRESSION*******');
+console.log("******** 2- EXPRESSION*******");
 
 //* Örnek1: Parametreli, Dönüş değerli
 //**********************************************************/
+
 // console.log(tekCift(5));
 //! function expression yöntemiyle bir fnk tanımlandığında önce tanımlamanın yapılması gerekir yoksa Uncaught ReferenceError: Cannot access 'tekCift' before initialization hatası alınır
 const tekCift = function (number) {
@@ -34,14 +36,14 @@ const buyukBul = function (x, y, z = -Number.MAX_VALUE) {
   }
   return enBuyuk;
 };
-console.log('En Büyük Sayi:', buyukBul(1, 3, 2));
-console.log('En Büyük Sayi:', buyukBul(5, 2, 1));
-console.log('En Büyük Sayi:', buyukBul(4, 6, 7));
-console.log('En Büyük Sayi:', buyukBul(6, 6, 7));
-console.log('En Büyük Sayi:', buyukBul(9, 8, 9));
-console.log('En Büyük Sayi:', buyukBul(8, 9, 9));
-console.log('En Büyük Sayi:', buyukBul(8, 9));
-console.log('En Büyük Sayi:', buyukBul(-8, -9));
+console.log("En Büyük Sayi:", buyukBul(1, 3, 2));
+console.log("En Büyük Sayi:", buyukBul(5, 2, 1));
+console.log("En Büyük Sayi:", buyukBul(4, 6, 7));
+console.log("En Büyük Sayi:", buyukBul(6, 6, 7));
+console.log("En Büyük Sayi:", buyukBul(9, 8, 9));
+console.log("En Büyük Sayi:", buyukBul(8, 9, 9));
+console.log("En Büyük Sayi:", buyukBul(8, 9));
+console.log("En Büyük Sayi:", buyukBul(-8, -9));
 
 //* ORNEK3: Bir fonksiyon içerisinde başka fonksiyonlar çağrılabilir
 //*****************************************************/
@@ -49,13 +51,13 @@ console.log('En Büyük Sayi:', buyukBul(-8, -9));
 const hesapla = function (islem, s1, s2) {
   let sonuc;
 
-  if (islem == '+') {
+  if (islem == "+") {
     sonuc = topla(s1, s2);
-  } else if (islem == '-') {
+  } else if (islem == "-") {
     sonuc = cikar(s1, s2);
-  } else if (islem == '*') {
+  } else if (islem == "*") {
     sonuc = carp(s1, s2);
-  } else if (islem == '/') {
+  } else if (islem == "/") {
     sonuc = bol(s1, s2);
   }
   return sonuc;
@@ -74,5 +76,5 @@ const bol = function (s1, s2) {
   return s1 / s2;
 };
 
-console.log(hesapla('+', 3, 5));
-console.log(hesapla('-', 3, 5));
+console.log(hesapla("+", 3, 5));
+console.log(hesapla("-", 3, 5));
