df656c0fd1aeca6497367a52a6b90cf7a76c3e8f
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-14 12:39:16 +0300

    Örnek1 update

--- JavaScript/Class-Notes/js05_functions/1-functionDeclaration.js
+++ JavaScript/Class-Notes/js05_functions/1-functionDeclaration.js
@@ -10,48 +10,47 @@ console.log('****** FUNC DECLARATION *********');
 
 //* Örnek1:
 //****************************************************************/
-yazdir(); //!invoke
-
+// yazdir(); //!invoke fonksiyon global olduğu için yukardada çalışır
 //! Declaration
 //! Paramatre almamış, bir şey döndürmek (void function)
-function yazdir() {
-  console.log('Merhaba');
-}
+// function yazdir() {
+//   console.log('Merhaba');
+// }
 
-yazdir(); //!invoke
-yazdir(); //!invoke
+// yazdir(); //!invoke
+// yazdir(); //!invoke
 
-console.log(typeof yazdir); //* function
+// console.log(typeof yazdir); //* function
 
 //* Örnek2: Parametreli Fonksiyon
 //***************************************************************/
 //! Bir parametreyi çağırma sırasında kullanmaz isek onun yerine default paramtere atayabiliriz. Önrketek lastName paramterinin default değerine '' atanmıştır.
 
-function selamla(name, lastName = '') {
-  console.log(`Merhaba ${name}  ${lastName}`);
-}
+// function selamla(name, lastName = '') {
+//   console.log(`Merhaba ${name}  ${lastName}`);
+// }
 
-selamla('Erhan', 'Yılmaz');
-selamla('Erol');
-selamla('jason');
+// selamla('Erhan', 'Yılmaz');
+// selamla('Erol');
+// selamla('jason');
 
 //* Örnek3: Parametreli, Dönüş değerli
 //***************************************************************/
 
-function yasHesapla(year, name) {
-  // const mesaj = `${name} in yaşı ${2022 - year} dir `;
-  const mesaj = `${name} in yaşı ${new Date().getFullYear() - year} dir `;
-  return mesaj;
-}
+// function yasHesapla(year, name) {
+// const mesaj = `${name} in yaşı ${2022 - year} dir `;
+//   const mesaj = `${name} in yaşı ${new Date().getFullYear() - year} dir `;
+//   return mesaj;
+// }
 
-const mesaj1 = yasHesapla(2001, 'Elif');
-console.log(mesaj1);
+// const mesaj1 = yasHesapla(2001, 'Elif');
+// console.log(mesaj1);
 
 //* Örnek4: Parametreli, Dönüş değerli
 //**********************************************************/
-console.log(tekCift(5));
-console.log(tekCift(2));
+// console.log(tekCift(5));
+// console.log(tekCift(2));
 
-function tekCift(number) {
-  return number % 2 === 0 ? `${number} çifttir` : `${number} tekdir`;
-}
+// function tekCift(number) {
+//   return number % 2 === 0 ? `${number} çifttir` : `${number} tekdir`;
+// }
