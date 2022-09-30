fbd3accb45c82ac9f5af6416e2f7a67eb5998d39
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-09-30 16:21:42 +0300

    bulb

---
+++ JavaScript/Assigments/js-ass5/bulb.js
@@ -0,0 +1,22 @@
+// const lamba = document.querySelector("img");
+// lamba.addEventListener("click", () => {
+//   if (lamba.src.match("pic_bulboff")) {
+//     lamba.src = "./img/pic_bulbon.gif";
+//   } else {
+//     lamba.src = "./img/pic_bulboff.gif";
+//   }
+// });
+
+
+const acık = document.querySelector("#on");
+const kapalı = document.querySelector("#off");
+const lamba = document.querySelector("img");
+acık.addEventListener("click", () => {
+  lamba.src = "./img/pic_bulbon.gif";
+//   console.log(acık);
+});
+
+kapalı.addEventListener("click", () => {
+  lamba.src = "./img/pic_bulboff.gif";
+//   console.log(kapalı);
+});
