5b2582acb3a75ebb84968a270edb51eaaa11b98e
omersb <omersaidbuldukk@gmail.com>
2022-11-09 16:10:39 +0300

    update

--- React/Class-Notes/rc11_styled-components-intro/src/components/Link.styled.jsx
+++ React/Class-Notes/rc11_styled-components-intro/src/components/Link.styled.jsx
@@ -1,20 +1,20 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
 const StyledLink = styled.a`
-  font-size: 1rem;
-  text-decoration: none;
-  color: white;
-  &:hover {
-    opacity: 0.5;
-  }
-  ${(props) =>
-    props.secondary &&
-    css`
-      background-color: yellow;
-      border-radius: 5px;
-      padding: 0.2rem;
-      color: black;
-    `}
+	font-size: 1rem;
+	text-decoration: none;
+	color: white;
+	&:hover {
+		opacity: 0.5;
+	}
+	${(props) =>
+		props.secondary &&
+		css`
+			background-color: yellow;
+			border-radius: 5px;
+			padding: 0.2rem;
+			color: black;
+		`}
 `;
 
 export default StyledLink;
