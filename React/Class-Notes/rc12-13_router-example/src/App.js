759a83a29a0f3e0fe48540373ec2d76da70c33f1
omersb <omersaidbuldukk@gmail.com>
2022-11-11 16:26:19 +0300

    update

--- React/Class-Notes/rc12-13_router-example/src/App.js
+++ React/Class-Notes/rc12-13_router-example/src/App.js
@@ -1,16 +1,16 @@
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Instructors from './pages/Instructors';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
 // import NotFound from './pages/NotFound';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import InstructorDetail from './pages/InstructorDetail';
-import { Navigate } from 'react-router-dom';
-import Paths from './pages/Paths';
-import FullStack from './pages/FullStack';
-import Aws from './pages/Aws';
-import PrivateRouter from './pages/PrivateRouter';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InstructorDetail from "./pages/InstructorDetail";
+import { Navigate } from "react-router-dom";
+import Paths from "./pages/Paths";
+import FullStack from "./pages/FullStack";
+import Aws from "./pages/Aws";
+import PrivateRouter from "./pages/PrivateRouter";
 
 //* İc ice sayfalari gsotermek icin Nested Route kullanilabilir.
 
@@ -28,34 +28,34 @@ import PrivateRouter from './pages/PrivateRouter';
 //* olarak yonledirme yapmak icin kullanilabilir.
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="instructors" element={<Instructors />} />
-          <Route path="instructors/:id" element={<InstructorDetail />} />
+	return (
+		<>
+			<BrowserRouter>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="instructors" element={<Instructors />} />
+					<Route path="instructors/:id" element={<InstructorDetail />} />
 
-          {/* Nested Route */}
-          <Route path="paths" element={<Paths />}>
-            <Route index element={<FullStack />} />
-            <Route path="fullstack" element={<FullStack />} />
-            <Route path="aws" element={<Aws />} />
-          </Route>
+					{/* Nested Route */}
+					<Route path="paths" element={<Paths />}>
+						<Route index element={<FullStack />} />
+						<Route path="fullstack" element={<FullStack />} />
+						<Route path="aws" element={<Aws />} />
+					</Route>
 
-          <Route path="contact" element={<PrivateRouter />}>
-            <Route path="" element={<Contact />} />
-          </Route>
+					<Route path="contact" element={<PrivateRouter />}>
+						<Route path="" element={<Contact />} />
+					</Route>
 
-          {/*
+					{/*
           <Route path="*" element={<NotFound />} /> */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+					<Route path="*" element={<Navigate to="/" />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
