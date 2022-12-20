b03e6ba39bab232d5645543d7fc6df8ae6b59174
omersb <omersaidbuldukk@gmail.com>
2022-12-20 22:54:07 +0300

    model added

--- Django-12/class-notes/002_Django_Models_1/fscohort/models.py
+++ Django-12/class-notes/002_Django_Models_1/fscohort/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    number = models.IntegerField(default=1111)
+    about = models.TextField(blank=True, null=True)
+    # auto_now_add: take the creation date and time
+    register = models.DateTimeField(auto_now_add=True)
+    # auto_now: take the update date and time
+    last_updated_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.number} + {self.first_name}"
+
+    class Meta:
+        ordering = ["number"]
+        verbose_name_plural = ["Student_list"]
+
+    def student_year_status(self):
+        "Returns the student's year status"
+        import datetime
+        if self.register_date < datetime.date(2019, 1, 1):
+            return "Senior"
+        if self.register_date < datetime.date(2019, 1, 1):
+            return "Junior"
+        else:
+            return "Freshman"
