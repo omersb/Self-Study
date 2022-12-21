52bbd11f195757aaa130e3cdf8cb62fab759d408
omersb <omersaidbuldukk@gmail.com>
2022-12-21 21:08:21 +0300

    update

--- Django-12/class-notes/003_Django_Models2/fscohort/models.py
+++ Django-12/class-notes/003_Django_Models2/fscohort/models.py
@@ -8,6 +8,15 @@ class Student(models.Model):
     last_name = models.CharField(max_length=40)
     number = models.PositiveSmallIntegerField(blank=True)
 
+    def __str__(self):
+        return f'{self.number} - {self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ('-number',)
+        verbose_name = 'Öğrenci'
+        verbose_name_plural = 'Öğrenciler'
+
+
     # about = models.TextField(blank=True, null=True)
     # register = models.DateTimeField(auto_now_add=True)
     # last_updated_date = models.DateTimeField(auto_now=True)
