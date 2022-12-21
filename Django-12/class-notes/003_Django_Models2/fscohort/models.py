771988ba4198a05553cc615c39b60b8fc97b9373
omersb <omersaidbuldukk@gmail.com>
2022-12-21 22:20:01 +0300

    models update

--- Django-12/class-notes/003_Django_Models2/fscohort/models.py
+++ Django-12/class-notes/003_Django_Models2/fscohort/models.py
@@ -5,6 +5,13 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     number = models.PositiveSmallIntegerField(blank=True, null=True)
+    about = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(default=True)
+    avatar = models.ImageField(blank=True, null=True, upload_to='student')
+    # files = models.FileField(blank=True, null=True, upload_to='student_files')
+    register_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.number} - {self.first_name} {self.last_name}'
