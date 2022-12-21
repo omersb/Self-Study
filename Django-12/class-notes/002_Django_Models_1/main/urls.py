10c89c8fb365c5e4c877d5d53ae52a3ea0260b85
omersb <omersaidbuldukk@gmail.com>
2022-12-21 19:54:24 +0300

    added

--- Django-12/class-notes/002_Django_Models_1/main/urls.py
+++ Django-12/class-notes/002_Django_Models_1/main/urls.py
@@ -1,7 +1,7 @@
 """main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fscohort/', include('fscohort.urls')),
+    path("fscohort/", include("fscohort.urls")),
 ]
