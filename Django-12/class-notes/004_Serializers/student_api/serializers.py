25789752ff7d23adf7dafa28772a33d81d4567db
omersb <omersaidbuldukk@gmail.com>
2022-12-22 23:08:47 +0300

    update

--- Django-12/class-notes/004_Serializers/student_api/serializers.py
+++ Django-12/class-notes/004_Serializers/student_api/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Path
 
 # class StudentSerializer(serializers.Serializer):
 #     first_name = serializers.CharField(max_length=50)
 #     last_name = serializers.CharField(max_length=50)
 #     number = serializers.IntegerField()
 #     age = serializers.IntegerField()
-
-
+    
+    
 #     def create(self, validated_data):
 #         return Student.objects.create(**validated_data)
 
@@ -21,13 +21,34 @@ from .models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
-
+    
+    born_year = serializers.SerializerMethodField()  # read_only
+    path = serializers.StringRelatedField() # read_only
+    path_id = serializers.IntegerField()
+    
     class Meta:
         model = Student
         # fields = "__all__"
-        fields = ["id", "first_name", "last_name", "number", "age"]
+        fields = ["id","first_name", "last_name","number", "age", "born_year", "path", "path_id"]
         # exclude = ["number"]
-
-# fields = "__all__" # tüm field kısımlarını alıyor
- # fields = ["firt_name","last_name"] # sadece istediğimiz filed ksımları alınıyor
- # exclude = ["number"]  # belirtilen field hariç geri kalanlar
+        
+    
+    def get_born_year(self, obj):
+        import datetime
+        current_time = datetime.datetime.now()
+        return current_time.year - obj.age
+    
+    
+class PathSerializer(serializers.ModelSerializer):
+    
+    # students = StudentSerializer(many=True)
+    students = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='detail'
+    )
+    
+    class Meta:
+        model = Path
+        # fields = "__all__"
+        fields = ["id", "path_name", "students"]
