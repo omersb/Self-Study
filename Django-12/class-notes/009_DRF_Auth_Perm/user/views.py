05f5388f0800aca7b6c55a92af256926c4533a3c
omersb <omersaidbuldukk@gmail.com>
2022-12-31 17:07:28 +0300

    update

--- Django-12/class-notes/009_DRF_Auth_Perm/user/views.py
+++ Django-12/class-notes/009_DRF_Auth_Perm/user/views.py
@@ -14,3 +14,13 @@ class RegisterView(CreateAPIView):
         response.data['token'] = token.key
         # print(response.data)
         return response
+
+# ----------- User Logout Function ------------------
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Kullanıcı Çıkış (Token Sil)
+@api_view(['POST'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"message": 'User Logout: Token Deleted'})
