bab0ee9f89ab3a96a65cd4fe5faf6a0b758e3f7f
omersb <omersaidbuldukk@gmail.com>
2023-03-31 18:22:30 +0300

    ts function

---
+++ TypeScript/ts04-Fonksiyonlar/function.js
@@ -0,0 +1,13 @@
+function topla(x, y) {
+    return x + y;
+}
+function topla2(x, y) {
+    return x + y;
+}
+var topla3 = function (x, y) {
+    return x + y;
+};
+console.log(topla(2, 3));
+console.log(topla("Ankara", 3));
+console.log(topla2(2, 4));
+console.log(topla3(4, 8));
---
+++ TypeScript/ts04-Fonksiyonlar/function.ts
@@ -0,0 +1,16 @@
+function topla(x,y) {
+  return x+y;
+}
+
+function topla2(x:number,y:number):number {
+  return x+y;
+}
+
+let topla3 = function(x:number,y:number):number {
+  return x+y;
+}
+
+console.log(topla(2,3));
+console.log(topla("Ankara",3));
+console.log(topla2(2,4));
+console.log(topla3(4,8));
