c9858aa6c35005a2c275e5771f063b0f0e1083d7
omersb <omersaidbuldukk@gmail.com>
2023-07-28 17:37:36 +0300

    router 404 sayfası

--- Nodejs/express-03/app.js
+++ Nodejs/express-03/app.js
@@ -5,9 +5,14 @@ const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+// routes
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
+app.use((req,res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
