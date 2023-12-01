9c5d686b8059c0136a3f97d812a5009cd513b9e2
omersb <omersaidbuldukk@gmail.com>
2023-12-01 19:23:05 +0300

    OOP-1

---
+++ Nodejs/Calisma-2/01-OOP_1/index.js
@@ -0,0 +1,8 @@
+let  node = 'Node.js'
+
+console.log(`----*---- 
+  ${node} 
+----*----`)
+
+// terminalde çalıştırmak için nodemon index.js
+
---
+++ Nodejs/Calisma-2/01-OOP_1/oop.js
@@ -0,0 +1,76 @@
+"use strict"
+
+/* ------------------------------
+    OBJECTS
+--------------------------------*/
+
+// const exampleObject = {
+//     propertyName: 'value',
+//     methodName: function() {
+//         return 'This is Method'
+//     }
+// }
+//
+// console.log(exampleObject.propertyName)
+// console.log(exampleObject.methodName())
+
+/* ------------------------------ */
+
+// const Car= {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 1967,
+//     isAutoGear: true,
+//     colors: ['white', 'red'],
+//     details: {
+//         color1: 'red',
+//         color2: 'white',
+//         engineSize: 4900
+//     },
+//     startEngine: function() {
+//         return 'Motor çalıştı'
+//     }
+// }
+
+// console.log(Car.brand)
+// console.log(Car.colors)
+// console.log(Car.colors[0])
+// console.log(Car.details)
+// console.log(Car.details.engineSize)
+// console.log(Car.startEngine())
+
+// console.log(Car['brand'])
+// console.log(Car.details['engineSize'])
+// console.log(Car['details']['engineSize'])
+// console.log(Car['startEngine']())
+
+/* ------------------------------ */
+//? 'this' keyword
+
+const Car= {
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    colors: ['white', 'red'],
+    details: {
+        color1: 'red',
+        color2: 'white',
+        engineSize: 4900
+    },
+    startEngine: function() {
+        return 'Motor çalıştı'
+    },
+    getDetails: function() {
+
+        // return this
+        // return this.brand + ' ' + this.model + ' ' + this.year
+        return this.startEngine()
+    },
+    arrowFunc: () => {
+        return this
+    }
+}
+
+// console.log(Car.getDetails())
+console.log(Car.arrowFunc())
