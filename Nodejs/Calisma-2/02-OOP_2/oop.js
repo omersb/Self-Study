e43bacab3947d2b3f73815f950a6c404051e6a73
omersb <omersaidbuldukk@gmail.com>
2023-12-01 23:02:48 +0300

    OOP-2

--- Nodejs/Calisma-2/02-OOP_2/oop.js
+++ Nodejs/Calisma-2/02-OOP_2/oop.js
@@ -42,4 +42,65 @@
 // console.log(instance.extraField)
 
 /* ----------------------------------------------- */
+//
+// class Car {
+//     isRunning = false
+//
+//     constructor(brand, model, year) {
+//         this.brand = brand
+//         this.model = model
+//         this.year = year
+//     }
+//
+//     runEngine() {
+//         this.isRunning = true
+//         console.log('Motor çalıştı')
+//         return this.isRunning
+//     }
+// }
+//
+// const ford = new Car('Ford', 'Mustang', 1967)
+// console.log(ford)
+// console.log(ford.isRunning)
+// ford.runEngine()
+// console.log(ford.isRunning)
+//
+// const mercedes = new Car('Mercedes', 'CLK200', 2000)
+// console.log(mercedes)
+
+/* ----------------------------------------------- */
+
+//? INHERITANCE: Miras alma. Basşka bir Class'ın tüm özellliklerini devralma (parent-child ilişkisi kurma)
+//? THIS: Chil Class - SUPER: Parent Class
+class Vehicle {
+    vehicleIsActive = false
+
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+}
+
+class Car extends Vehicle {
+    isRunning = false
+
+    constructor(brand, model, year, vehicleType = 'Car') {
+        //? super must be top
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Motor çalıştı')
+        return this.isRunning
+    }
+}
+
+const ford = new Car('Ford', 'Mustang', 1967)
+console.log(ford)
+
+
+/* ----------------------------------------------- */
 
