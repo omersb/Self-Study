71bd16521e6b67d0b2dc0af1e3ea6efc30bf77e0
omersb <omersaidbuldukk@gmail.com>
2023-12-01 19:32:39 +0300

    OOP-1

--- Nodejs/Calisma-2/01-OOP_1/oop.js
+++ Nodejs/Calisma-2/01-OOP_1/oop.js
@@ -45,32 +45,54 @@
 // console.log(Car['startEngine']())
 
 /* ------------------------------ */
-//? 'this' keyword
 
-const Car= {
-    brand: 'Ford',
-    model: 'Mustang',
-    year: 1967,
-    isAutoGear: true,
-    colors: ['white', 'red'],
-    details: {
-        color1: 'red',
-        color2: 'white',
-        engineSize: 4900
-    },
-    startEngine: function() {
-        return 'Motor çalıştı'
-    },
-    getDetails: function() {
+// //? 'this' keyword
+//
+// const Car = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 1967,
+//     isAutoGear: true,
+//     colors: ['white', 'red'],
+//     details: {
+//         color1: 'red',
+//         color2: 'white',
+//         engineSize: 4900
+//     },
+//     startEngine: function () {
+//         return 'Motor çalıştı'
+//     },
+//     getDetails: function () {
+//
+//         // return this
+//         // return this.brand + ' ' + this.model + ' ' + this.year
+//         return this.startEngine()
+//     },
+//     arrowFunc: () => {
+//         //? Arrow function içinde 'this' keyword kullanılamaz
+//         // return this
+//         return this.brand
+//     }
+// }
+//
+// // console.log(Car.getDetails())
+// console.log(Car.arrowFunc())
 
-        // return this
-        // return this.brand + ' ' + this.model + ' ' + this.year
-        return this.startEngine()
-    },
-    arrowFunc: () => {
-        return this
-    }
-}
+/* ------------------------------ */
 
-// console.log(Car.getDetails())
-console.log(Car.arrowFunc())
+// //? Array Destructuring
+//
+// const testArray = ['value0', 'value1', 'value2', 'value3']
+//
+// // const var0 = testArray[0]
+// // const var1 = testArray[1]
+//
+// //? Sıralma önemli!
+// const [firstItem, secondItem] = testArray
+// console.log(firstItem, secondItem)
+//
+// //? SpreadOperator (En sonda olmak zorunda)
+// const [first, second, ...others] = testArray
+// console.log(first, second, others)
+
+/* ------------------------------ */
