98aa1ff5515626aa90af820c080bcbe88f255e2f
omersb <omersaidbuldukk@gmail.com>
2023-12-01 19:51:14 +0300

    OOP-1

--- Nodejs/Calisma-2/01-OOP_1/oop.js
+++ Nodejs/Calisma-2/01-OOP_1/oop.js
@@ -88,11 +88,70 @@
 // // const var1 = testArray[1]
 //
 // //? Sıralma önemli!
-// const [firstItem, secondItem] = testArray
-// console.log(firstItem, secondItem)
+// // const [firstItem, secondItem] = testArray
+// // console.log(firstItem, secondItem)
 //
 // //? SpreadOperator (En sonda olmak zorunda)
-// const [first, second, ...others] = testArray
-// console.log(first, second, others)
+// // const [first, second, ...others] = testArray
+// // console.log(first, second, others)
+//
+// //? SpreadOperator (Dağıtıcı)
+// const newArr = [...testArray, 'new-value', 'new-value2']
+// console.log(newArr)
+
+/* ------------------------------ */
+
+// //? Object Destructuring
+//
+// const Car = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     year: 1967,
+//     isAutoGear: true,
+//     colors: ['white', 'red'],
+//     details: {
+//         color1: 'red',
+//         color2: 'white',
+//         engineSize: 4900
+//     },
+//     startEngine: function () {
+//         return 'Motor çalıştı'
+//     }
+// }
+//
+// //Rest:
+// // const {year, model, brand} = Car
+// // const {year, model, brand, ...otherItems} = Car
+// // console.log(year, model, brand)
+// // console.log(otherItems)
+//
+// // const {year: modelYear, model: newName, brand, ...otherItems} = Car
+// // console.log(modelYear, newName, brand)
+// // console.log(otherItems)
+// // console.log(Car) // Car objesi değişmez
+//
+// //Spread:
+// const newObj = {
+//     ...Car,
+//     newKey: 'new value'
+// }
+// // console.log(newObj)
+//
+// // Object to JSON
+// const json = JSON.stringify(Car)
+// // console.log(typeof json, json)
+//
+// // JSON to Object
+// const newObj2 = JSON.parse(json)
+// // console.log(typeof newObj2, newObj2)
+//
+// // Object to Array
+// // const arr = [...Car]
+// const arr = [ ...Object.entries(Car)]
+// // console.log(arr)
+// const arr2 = Object.values(Car)
+// console.log(arr2)
+// const arr3 = Object.keys(Car)
+// console.log(arr3)
 
 /* ------------------------------ */
