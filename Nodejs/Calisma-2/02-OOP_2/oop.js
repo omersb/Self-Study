3d4eb20c9740003e59012ed7a6cf721fc8638b4d
omersb <omersaidbuldukk@gmail.com>
2023-12-01 22:41:58 +0300

    OOP-2

--- Nodejs/Calisma-2/02-OOP_2/oop.js
+++ Nodejs/Calisma-2/02-OOP_2/oop.js
@@ -1,6 +1,45 @@
 "use strict"
 
-
-/* ------------------------------
+/* -----------------------------------------------
     OOP & Classes
---------------------------------*/
+----------------------------------------------- */
+
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılan şablon
+
+// Class Declaration
+// class PascalNamedClassName{...}
+
+// Class Expression
+// const PascalNamedClassName = class {
+//
+//     undefinedProperty // onle definition('undefined')
+//     extraField = 'field-value'
+//
+//     //? "new Class" ile obje oluştururken veri göndermek için "constructor" methodu kullanılır
+//     constructor(parameter1, parameter2 = 'default-value') {
+//         this.parameter1 = parameter1
+//         this.parameter2 = parameter2
+//     }
+//
+//     methodName1() {
+//         return this
+//     }
+//
+//     methodName2() {
+//         return this.extraField
+//
+//     }
+// }
+//
+// //? INSTANCE: Bir classtan türetilen objedir
+// const instance = new PascalNamedClassName('parameter-1-value', 'parameter-2-value')
+// console.log(instance)
+// console.log(instance.methodName2())
+// instance.extraField = 'new-value'
+// console.log(instance.extraField)
+
+/* ----------------------------------------------- */
+
