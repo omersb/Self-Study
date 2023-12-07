701101e51eae96334d62dfd423a9e17505fb2252
omersb <omersaidbuldukk@gmail.com>
2023-12-07 16:27:58 +0300

    routing

--- Nodejs/Calisma-2/05-Routers_Errors/routes/user.js
+++ Nodejs/Calisma-2/05-Routers_Errors/routes/user.js
@@ -4,6 +4,36 @@
 ------------------------------------------------------ */
 
 const router = require("express").Router();
+
+const routeControl = (req, res, next) => {
+
+    const {username} = req.query;
+
+    if (username == "admin") {
+        next()
+    } else {
+        res.send({
+            message: "Word Username or Password"
+        })
+    }
+}
+
+// We can use middleware with router
+router.use(routeControl);
+router.route('/extra')
+    .get((req, res) => {
+        res.send({message: "get"})
+    })
+    .post((req, res) => {
+        res.send({message: "post"})
+    })
+    .put((req, res) => {
+        res.send({message: "put"})
+    })
+    .delete((req, res) => {
+        res.send({message: "delete"})
+    })
+
 router.get('/', (req, res) => {
     res.send({message: "All Users"})
 });
@@ -19,5 +49,6 @@ router.get('/:userId', (req, res) => {
 router.get('/:userId/password', (req, res) => {
     res.send({message: "Password Page"})
 });
+
 
 module.exports = router;
