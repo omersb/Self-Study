decef8417903adbf4caeaf4691f7bdd5ce5c9a06
omersb <omersaidbuldukk@gmail.com>
2023-12-07 16:59:23 +0300

    errors

--- Nodejs/Calisma-2/05-Routers_Errors/errors.js
+++ Nodejs/Calisma-2/05-Routers_Errors/errors.js
@@ -2,20 +2,84 @@
 /* ------------------------------------------------------
     ExpressJS - error management
 ------------------------------------------------------ */
+
 /*
 * npm init -y
 * npm i express dotenv
 * npm i express-async-handler
 */
+
 const express = require('express');
 const app = express();
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------ */
+
+// app.get('/user/:id', (req, res) => {
+//
+//     const id = req.params.id ?? 0;
+//     if (isNaN(id)) {
+//         res.statusCode = 400;
+//         throw new Error('ID is Not A Number', {cause: 'params.id=' + id});
+//     } else {
+//         res.send({
+//             error: false,
+//             id: id
+//         });
+//     }
+// });
+
+/* ------------------------------------------------------ */
+// Try-Catch
+
+app.get('/user/:id', (req, res, next) => {
+
+    try {
+        const id = req.params.id ?? 0;
+        if (isNaN(id)) {
+            throw new Error('ID is Not A Number', {cause: 'params.id=' + id});
+        } else {
+            res.send({
+                error: false,
+                id: id
+            });
+        }
+    } catch (err) {
+        console.log('catch çalıştı')
+        next(err); // go to errorHandler
+
+        // res.send({
+        //     error: true,
+        //     message: err.message,
+        //     cause: err.cause
+        // })
+    }
+});
+
+
 /* ------------------------------------------------------ */
 
 
+/* ------------------------------------------------------ */
+//? ErrorHandler kodlamanın en sonunda yer almalıdır.
 
+const errorHandler = (err, req, res, next) => {
+
+    const statusCode = res.statusCode ?? 500
+
+    console.log('errorHandler Çalıştı')
+
+    res.status(statusCode).send({
+        error: true,
+        message: err.message,
+        cause: err.cause
+    })
+
+}
+
+app.use(errorHandler);
 
 
 /* ------------------------------------------------------ */
