58e3f04176a4f7424b0197505a74bbf9e1e4ddae
omersb <omersaidbuldukk@gmail.com>
2023-12-07 16:02:53 +0300

    routing

---
+++ Nodejs/Calisma-2/05-Routers_Errors/index.js
@@ -0,0 +1,37 @@
+"use strict"
+/* ------------------------------------------------------
+    ExpressJS - Routing
+------------------------------------------------------ */
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------ */
+//? "Router" is special app for URL control in ExpressJS
+
+// app.get('/', (req, res) => { res.send({message: "Home Page"}) });
+// app.get('/about', (req, res) => { res.send({message: "About Page"}) });
+// app.get('/user/:userId', (req, res) => { res.send({message: "User Page"}) });
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.send({message: "Home Page"})
+});
+router.get('/about', (req, res) => {
+    res.send({message: "About Page"})
+});
+router.get('/user/:userId', (req, res) => {
+    res.send({message: "User Page"})
+});
+
+app.use(router);
+
+
+
+
+
+/* ------------------------------------------------------ */
+app.listen(PORT, () => console.log("Runnng: http://127.0.0.1:" + PORT));
