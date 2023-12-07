083e6d7985ea07c9cf770f4fe1e5affebe21bbd7
omersb <omersaidbuldukk@gmail.com>
2023-12-07 17:10:50 +0300

    errors

--- Nodejs/Calisma-2/05-Routers_Errors/errors.js
+++ Nodejs/Calisma-2/05-Routers_Errors/errors.js
@@ -34,32 +34,52 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------ */
 // Try-Catch
 
-app.get('/user/:id', (req, res, next) => {
-
-    try {
-        const id = req.params.id ?? 0;
-        if (isNaN(id)) {
-            throw new Error('ID is Not A Number', {cause: 'params.id=' + id});
-        } else {
-            res.send({
-                error: false,
-                id: id
-            });
-        }
-    } catch (err) {
-        console.log('catch çalıştı')
-        next(err); // go to errorHandler
-
-        // res.send({
-        //     error: true,
-        //     message: err.message,
-        //     cause: err.cause
-        // })
-    }
-});
+// app.get('/user/:id', (req, res, next) => {
+//
+//     try {
+//         const id = req.params.id ?? 0;
+//         if (isNaN(id)) {
+//             throw new Error('ID is Not A Number', {cause: 'params.id=' + id});
+//         } else {
+//             res.send({
+//                 error: false,
+//                 id: id
+//             });
+//         }
+//     } catch (err) {
+//         console.log('catch çalıştı')
+//         next(err); // go to errorHandler
+//
+//         // res.send({
+//         //     error: true,
+//         //     message: err.message,
+//         //     cause: err.cause
+//         // })
+//     }
+// });
 
 
 /* ------------------------------------------------------ */
+// Async
+
+// const asyncFunction = async () => {
+//     throw new Error('Created error in async-func')
+// }
+//
+// app.get('/async', async (req, res, next) => {
+//     await asyncFunction().catch(next)
+// })
+
+/* ------------------------------------------------------ */
+// express-async-handler
+// npm i express-async-handler
+
+const asyncHandler = require('express-async-handler')
+
+app.get('/async', asyncHandler(async (req, res, next) => {
+    res.errorStatusCode = 400;
+    throw new Error('Created error in async-func')
+}))
 
 
 /* ------------------------------------------------------ */
@@ -67,7 +87,7 @@ app.get('/user/:id', (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
 
-    const statusCode = res.statusCode ?? 500
+    const statusCode = res.errorStatusCode ?? 500;
 
     console.log('errorHandler Çalıştı')
 
@@ -75,6 +95,7 @@ const errorHandler = (err, req, res, next) => {
         error: true,
         message: err.message,
         cause: err.cause
+        // stack: err.stack // error details
     })
 
 }
