8f76eb06c73ccae7d71e6802e040bf751d513b97
omersb <omersaidbuldukk@gmail.com>
2023-12-12 14:21:38 +0300

    blogapi

---
+++ Nodejs/Calisma-2/08-BlogAPI/index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+/* ------------------------------------------------------
+    ExpressJS - Blog Project with Mongoose
+------------------------------------------------------ */
+
+/*
+* npm init -y
+* npm i express dotenv express-async-errors
+* npm i mongoose
+*/
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------ */
+
+
+/* ------------------------------------------------------ */
+app.listen(PORT, () => console.log("Runnng: http://127.0.0.1:" + PORT));
